48ef9c237b982e841a26780bb9a74127aa57ff18
SofiaPigino <sofipigino98@gmail.com>
2021-02-08 16:49:36 -0300

    Section 1 - 4

---
+++ src/main/java/com/sofia/springboot/backend/apirest/models/services/ClienteServiceImpl.java
@@ -0,0 +1,25 @@
+package com.sofia.springboot.backend.apirest.models.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.sofia.springboot.backend.apirest.models.entity.Cliente;
+import com.sofia.springboot.backend.apirest.models.dao.IClienteDao;
+
+@Service
+public class ClienteServiceImpl implements IClienteService{
+
+	@Autowired
+	private IClienteDao clienteDao;
+	@Override
+	@Transactional(readOnly = true)
+	public List<Cliente> findAll() {
+		return (List<Cliente>) clienteDao.findAll();
+	}
+	
+	
+
+}
