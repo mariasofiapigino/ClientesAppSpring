abb327290dca29e8c28722db0ca81dbd62636726
SofiaPigino <sofipigino98@gmail.com>
2021-02-09 19:15:35 -0300

    Section 5

--- src/main/java/com/sofia/springboot/backend/apirest/models/services/ClienteServiceImpl.java
+++ src/main/java/com/sofia/springboot/backend/apirest/models/services/ClienteServiceImpl.java
@@ -20,6 +20,24 @@ public class ClienteServiceImpl implements IClienteService{
 		return (List<Cliente>) clienteDao.findAll();
 	}
 	
+	@Override
+	@Transactional(readOnly = true)
+	public Cliente findById(Long id) {
+		return clienteDao.findById(id).orElse(null);
+	}
+	
+	@Override
+	@Transactional
+	public Cliente save(Cliente cliente) {
+		return clienteDao.save(cliente);
+	}
+	
+	@Transactional
+	@Override
+	public void delete(Long id) {
+		clienteDao.deleteById(id);
+	}
+	
 	
 
 }
